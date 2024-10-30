706942a25517407e55ff0ac9b7326dc19b57c8f8
Admin <Admin@QUANGVINH>
2024-10-30 22:01:15 +0700

    Add project files.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 592);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(900, 63);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "button1";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartGame);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 616);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging; // add this for the JPG compressor
+
+namespace Snake
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            new Settings();
+        }
+        List<Circles> Snake=new List<Circles>(); //Tạo list chứa các Circles
+        private Circles food = new Circles(); //Tạo biến food
+        int maxWidth; //Chiều rộng tối đa của map
+        int maxHeight; //Chiều dài tối đa của map
+        int score; //Điểm
+        int highScore; //Điểm kỉ lục
+        Random rand = new Random();
+        bool goLeft, goRight, goDown, goUp; //các biến bool mũi tên
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void KeyIsDown(object sender, KeyEventArgs e) //Nhấn xuống
+        {
+            if (e.KeyCode == Keys.Left && Settings.directions != "right")
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right && Settings.directions != "left")
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up && Settings.directions != "down")
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down && Settings.directions != "up")
+            {
+                goDown = true;
+            }
+        }
+        private void KeyIsUp(object sender, KeyEventArgs e) //Nhả ra
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+        }
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (goLeft)
+            {
+                Settings.directions = "left";
+            }
+            else if (goRight)
+            {
+                Settings.directions = "right";
+            }
+            else if (goDown)
+            {
+                Settings.directions = "down";
+            }
+            else if (goUp)
+            {
+                Settings.directions = "up";
+            }
+            // end of directions
+            for (int i = Snake.Count - 1; i >= 0; i--) //Duyệt từ đuôi, xuống else để hiểu
+            {
+                if (i == 0) //Trường hợp duyệt tới đầu
+                {
+                    switch (Settings.directions) //Các trường hợp directions, vẽ trục tọa độ ta sẽ rõ
+                    {
+                        case "left":
+                            Snake[i].X--; 
+                            break;
+                        case "right":
+                            Snake[i].X++;
+                            break;
+                        case "down":
+                            Snake[i].Y++;
+                            break;
+                        case "up":
+                            Snake[i].Y--;
+                            break;
+                    }
+                    //Các trường hợp chạm tường
+                    if (Snake[i].X < 0)
+                    {
+                        Snake[i].X = maxWidth;
+                    }
+                    if (Snake[i].X > maxWidth)
+                    {
+                        Snake[i].X = 0;
+                    }
+                    if (Snake[i].Y < 0)
+                    {
+                        Snake[i].Y = maxHeight;
+                    }
+                    if (Snake[i].Y > maxHeight)
+                    {
+                        Snake[i].Y = 0;
+                    }
+                    //Trường hợp ăn trái cây, để ở dưới các nút và các trường hợp xuyên tường
+                    if (Snake[i].X == food.X && Snake[i].Y == food.Y)
+                    {
+                        EatFood();
+                    }
+                    //Trường hợp chạm vào thân
+                    for (int j = 1; j < Snake.Count; j++)
+                    {
+                        if (Snake[i].X == Snake[j].X && Snake[i].Y == Snake[j].Y)
+                        {
+                            GameOver();
+                        }
+                    }
+                }
+                else //Phần tử sau bằng phần tử trước, lấy tọa độ mới
+                {
+                    Snake[i].X = Snake[i - 1].X; 
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+            }
+            pictureBox1.Invalidate(); //Invalidate để picturebox vẽ lại
+        }
+        private void StartGame(object sender, EventArgs e) //Button start
+        {
+            RestartGame();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) //Vẽ trên picturebox
+        {
+            Graphics canvas = e.Graphics;
+            Brush snakeColour; 
+            for (int i = 0; i < Snake.Count; i++)
+            {
+                if (i == 0)
+                {
+                    snakeColour = Brushes.Red; //Vẽ đầu rắng màu đỏ cho oách xà lách
+                }
+                else
+                {
+                    snakeColour = Brushes.Black; //thân rắn màu đen cho nổi
+                }
+                canvas.FillEllipse(snakeColour, new Rectangle 
+                    (
+                    Snake[i].X * Settings.Width,
+                    Snake[i].Y * Settings.Height,
+                    Settings.Width, Settings.Height
+                    )); //Vẽ rắn là những hình Ellipse nối đuôi nhau, sau thay bằng Image khác
+            }
+            canvas.FillEllipse(Brushes.DarkRed, new Rectangle
+            (
+            food.X * Settings.Width,
+            food.Y * Settings.Height,
+            Settings.Width, Settings.Height
+            )); //Vẽ đồ ăn
+        }
+        private void AppearFood() //Hàm xuất hiện đồ ăn, loại trường hợp đồ ăn hiện ngay thân rắn
+        {
+            food = new Circles { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+            bool k = true;
+            for (int i = 0; i < Snake.Count; i++)
+            {
+                if (food.X == Snake[i].X && food.Y == Snake[i].Y) { k = false; break; }
+            }
+            if(!k) AppearFood();
+        }
+        private void EatFood() //Rắn ăn 1 quả sẽ dài thêm 1 nấc => list snake sẽ add 1 phần tử mới có tọa độ bằng tọa độ phần tử cuối của list cũ
+        {
+            score += 1; //Cộng điểm
+            Circles body = new Circles
+            {
+                X = Snake[Snake.Count - 1].X,
+                Y = Snake[Snake.Count - 1].Y
+            };
+            Snake.Add(body);
+            AppearFood();
+        }
+        private void RestartGame()
+        {
+            //Tính toán max dài và rộng của picturebox
+            maxWidth = pictureBox1.Width / Settings.Width - 1;
+            maxHeight = pictureBox1.Height / Settings.Height - 1;
+            Snake.Clear(); //Xóa hết dữ liệu của snake cũ
+            startButton.Enabled = false; 
+            score = 0;
+            //Tạo 1 snake mới gồm 1 đầu và 1 nấc thân
+            Circles head = new Circles { X = 10, Y = 5 };
+            Snake.Add(head); 
+            for (int i = 0; i < 1; i++)
+            {
+                Circles body = new Circles();
+                Snake.Add(body);
+            }
+            AppearFood(); //Đồ ăn xuất hiện
+            timer1.Start(); //Timer chạy
+        }
+        private void GameOver()
+        {
+            startButton.Enabled = true;
+            timer1.Stop();
+        }
+    }
+}
